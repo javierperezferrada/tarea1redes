0f7c351e27a45729a9fdbb39e11e21c067c3dd62
javierperezferrada <javier_dc2@hotmail.com>
2015-10-24 12:58:54 -0300

    primer commit

---
+++ placa_ethernet/placa_ethernet.ino
@@ -0,0 +1,18 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
+
+
+void setup() {
+  Serial.begin(9600);
+  // initialize the ethernet device
+  Ethernet.begin(mac);
+  //print out the IP address
+  Serial.print("IP = ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
